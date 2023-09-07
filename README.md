a2878ef104ab4f3f291924a57f89b36067202b50
benjizhai <ben@zhai.ch>
2023-09-07 22:52:03 +0200

    chore: refactor
    Signed-off-by: benjizhai <ben@zhai.ch>

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # ckICP Main Canister
 ckICP Main canister on the Internet Computer.
 
+# Architecture
+The ICP part consists of 2 canisters: a *minter* canister, and an *eth_state* canister.
+
+## Minter Canister
+This canister is responsible for issuing minting signatures using tECDSA, and transferring ICP when it gets
+notifications about burned ckICP from the *eth_state* canister.
+
+## ETH State Sync Canister
+This canister is responsible for syncing states from Ethereum, and notifying the minter.
+
 # License
 MIT
--- rs/minter/src/lib.rs
+++ rs/minter/src/lib.rs
@@ -20,6 +20,8 @@ use rustic::types::*;
 use rustic::utils::*;
 
 use serde_bytes::ByteBuf;
+use zeroize::ZeroizeOnDrop;
+
 use std::borrow::Cow;
 use std::cell::RefCell;
 use std::convert::From;
@@ -32,6 +34,7 @@ use k256::{
     },
     Secp256k1,
     PublicKey,
+    ecdsa::VerifyingKey,
 };
 
 use icrc_ledger_types::icrc1::account::{Account, Subaccount};
@@ -132,17 +135,10 @@ thread_local! {
 
 #[init]
 pub fn init() {
-   setup_timers();
+   
 }
 
-fn setup_timers() {
-    ic_cdk_timers::set_timer(Duration::from_secs(0), || {
-        // Initialize the minter's public key to make the address known.
-        ic_cdk::spawn(async {
-            let _ = lazy_call_ecdsa_public_key().await;
-        })
-    });
-}
+
 
 #[query]
 pub fn get_ckicp_config() -> CkicpConfig {
@@ -175,44 +171,6 @@ pub fn mint_ckicp(amount: Amount, target_eth_wallet: [u8;20]) -> Result<EcdsaSig
 }
 
 
-pub async fn lazy_call_ecdsa_public_key() -> PublicKey {
-    use ic_cdk::api::management_canister::ecdsa::{
-        ecdsa_public_key, EcdsaCurve, EcdsaKeyId, EcdsaPublicKeyArgument,
-    };
-
-    fn to_public_key(response: &EcdsaPublicKeyResponse) -> PublicKey {
-        PublicKey::deserialize_sec1(&response.public_key).unwrap_or_else(|e| {
-            ic_cdk::trap(&format!("failed to decode minter's public key: {:?}", e))
-        })
-    }
-
-    if let Some(ecdsa_pk_response) = read_state(|s| s.ecdsa_public_key.clone()) {
-        return to_public_key(&ecdsa_pk_response);
-    }
-    let key_name = read_state(|s| s.ecdsa_key_name.clone());
-    log!(DEBUG, "Fetching the ECDSA public key {key_name}");
-    let (response,) = ecdsa_public_key(EcdsaPublicKeyArgument {
-        canister_id: None,
-        derivation_path: crate::MAIN_DERIVATION_PATH
-            .into_iter()
-            .map(|x| x.to_vec())
-            .collect(),
-        key_id: EcdsaKeyId {
-            curve: EcdsaCurve::Secp256k1,
-            name: key_name,
-        },
-    })
-    .await
-    .unwrap_or_else(|(error_code, message)| {
-        ic_cdk::trap(&format!(
-            "failed to get minter's public key: {} (error code = {:?})",
-            message, error_code,
-        ))
-    });
-    mutate_state(|s| s.ecdsa_public_key = Some(response.clone()));
-    to_public_key(&response)
-}
-
 
 /// An ECDSA private key
 #[derive(Clone, ZeroizeOnDrop)]
@@ -221,65 +179,6 @@ pub struct PrivateKey {
 }
 
 impl PrivateKey {
-    /// Generate a new random private key
-    pub fn generate() -> Self {
-        let mut rng = rand::thread_rng();
-        Self::generate_using_rng(&mut rng)
-    }
-
-    /// Generate a new random private key using some provided RNG
-    pub fn generate_using_rng<R: RngCore + CryptoRng>(rng: &mut R) -> Self {
-        let key = k256::ecdsa::SigningKey::random(rng);
-        Self { key }
-    }
-
-    /// Deserialize a private key encoded in SEC1 format
-    pub fn deserialize_sec1(bytes: &[u8]) -> Result<Self, KeyDecodingError> {
-        let byte_array: [u8; <Secp256k1 as Curve>::FieldBytesSize::USIZE] =
-            bytes.try_into().map_err(|_e| {
-                KeyDecodingError::InvalidKeyEncoding(format!("invalid key size = {}.", bytes.len()))
-            })?;
-
-        let key = k256::ecdsa::SigningKey::from_bytes(&GenericArray::from(byte_array))
-            .map_err(|e| KeyDecodingError::InvalidKeyEncoding(format!("{:?}", e)))?;
-        Ok(Self { key })
-    }
-
-    /// Deserialize a private key encoded in PKCS8 format
-    pub fn deserialize_pkcs8_der(der: &[u8]) -> Result<Self, KeyDecodingError> {
-        use k256::pkcs8::DecodePrivateKey;
-        let key = k256::ecdsa::SigningKey::from_pkcs8_der(der)
-            .map_err(|e| KeyDecodingError::InvalidKeyEncoding(format!("{:?}", e)))?;
-        Ok(Self { key })
-    }
-
-    /// Deserialize a private key encoded in PKCS8 format with PEM encoding
-    pub fn deserialize_pkcs8_pem(pem: &str) -> Result<Self, KeyDecodingError> {
-        let der = pem::parse(pem)
-            .map_err(|e| KeyDecodingError::InvalidPemEncoding(format!("{:?}", e)))?;
-        if der.tag != PEM_HEADER_PKCS8 {
-            return Err(KeyDecodingError::UnexpectedPemLabel(der.tag));
-        }
-
-        Self::deserialize_pkcs8_der(&der.contents)
-    }
-
-    /// Deserialize a private key encoded in RFC 5915 format
-    pub fn deserialize_rfc5915_der(der: &[u8]) -> Result<Self, KeyDecodingError> {
-        let key = der_decode_rfc5915_privatekey(der)?;
-        Self::deserialize_sec1(&key)
-    }
-
-    /// Deserialize a private key encoded in RFC 5915 format with PEM encoding
-    pub fn deserialize_rfc5915_pem(pem: &str) -> Result<Self, KeyDecodingError> {
-        let der = pem::parse(pem)
-            .map_err(|e| KeyDecodingError::InvalidPemEncoding(format!("{:?}", e)))?;
-        if der.tag != PEM_HEADER_RFC5915 {
-            return Err(KeyDecodingError::UnexpectedPemLabel(der.tag));
-        }
-        Self::deserialize_rfc5915_der(&der.contents)
-    }
-
     /// Serialize the private key to a simple bytestring
     ///
     /// This uses the SEC1 encoding, which is just the representation
@@ -289,27 +188,6 @@ impl PrivateKey {
         self.key.to_bytes().to_vec()
     }
 
-    /// Serialize the private key as PKCS8 format in DER encoding
-    pub fn serialize_pkcs8_der(&self) -> Vec<u8> {
-        der_encode_pkcs8_rfc5208_private_key(&self.serialize_sec1())
-    }
-
-    /// Serialize the private key as PKCS8 format in PEM encoding
-    pub fn serialize_pkcs8_pem(&self) -> String {
-        pem_encode(&self.serialize_pkcs8_der(), PEM_HEADER_PKCS8)
-    }
-
-    /// Serialize the private key as RFC 5915 in DER encoding
-    pub fn serialize_rfc5915_der(&self) -> Vec<u8> {
-        let sk = self.serialize_sec1();
-        let pk = self.public_key().serialize_sec1(false);
-        der_encode_rfc5915_privatekey(&sk, true, Some(pk))
-    }
-
-    /// Serialize the private key as RFC 5915 in PEM encoding
-    pub fn serialize_rfc5915_pem(&self) -> String {
-        pem_encode(&self.serialize_rfc5915_der(), PEM_HEADER_RFC5915)
-    }
 
     /// Sign a message
     ///
@@ -340,7 +218,8 @@ impl PrivateKey {
 
     /// Return the public key cooresponding to this private key
     pub fn public_key(&self) -> PublicKey {
-        let key = self.key.verifying_key();
-        PublicKey { key: *key }
+        let key = VerifyingKey::from(&self.key);
+        PublicKey::from(&key)
+        
     }
 }
